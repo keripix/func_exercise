242e95eea960becce0fe6faf70ed881f284a0044
A. Akbar Hidayat <diqo@dicoding.com>
2016-02-28 15:00:45 +0700

    written some comments

--- test/func_exercise/redmap_test.clj
+++ test/func_exercise/redmap_test.clj
@@ -6,8 +6,8 @@
          "implementing map with reduce"
          (is (= [1 2 3 4]
                 (reduce (fn [x y]
-                            (if (coll? x)
-                              (conj (vec x) (inc y))
-                              (vector (inc x) (inc y))))
+                            (if (coll? x)                   ; when the first argument does not implement IPersistentCollection, then we can asume that we are iterating on the first 2 items
+                              (conj (vec x) (inc y))        ;
+                              (vector (inc x) (inc y))))    ; creating a vector with x and y after applying inc function to both of them
                         [0 1 2 3])
                 )))
