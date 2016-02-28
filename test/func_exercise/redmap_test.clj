a39c5312507452d9394cd9dc11c19a30ede044d6
A. Akbar Hidayat <diqo@dicoding.com>
2016-02-28 15:22:05 +0700

    created an abstracted function

--- test/func_exercise/redmap_test.clj
+++ test/func_exercise/redmap_test.clj
@@ -2,6 +2,10 @@
   (:require [clojure.test :refer :all]
             [clojure.core :refer :all]))
 
+(defn mapping_using_reduce
+  [fn coll]
+  (reduce #(conj (vec %1) (fn %2)) [] coll))
+
 (deftest test-redmap
          "implementing map with reduce"
          (is (= [1 2 3 4]
@@ -10,4 +14,6 @@
                               (conj (vec x) (inc y))        ;
                               (vector (inc x) (inc y))))    ; creating a vector with x and y after applying inc function to both of them
                         [0 1 2 3])
-                )))
+                ))
+         "abstracting the above steps to a function"
+         (is (= [1 2 3 4] (mapping_using_reduce #(inc %1) [0 1 2 3]))))
